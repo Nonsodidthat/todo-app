9b0eeee0b39f6cf1efce57937c7f4b54cf3617ad
Chukwunonso <nemelumadu10@gmail.com>
2025-03-19 15:23:06 +0100

    Implemented 'Edit' feature

--- Gui.py
+++ Gui.py
@@ -2,11 +2,14 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a To-Do")
-input_box = sg.InputText(tooltip= "enter to-do", key= "tasks")
+input_box = sg.InputText(tooltip= "enter to-do", key= "task")
 new_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_tasks(), key= "tasks",
+                      enable_events= True, size=(25,15))
+new_button2 = sg.Button("Edit")
 
 window = sg.Window("My to-do app",
-                   layout=[[label], [input_box], [new_button]],
+                   layout=[[label], [input_box], [new_button], [list_box,new_button2]],
                    font= ('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -15,9 +18,21 @@ while True:
     match event:
         case 'Add':
             tasks = functions.get_tasks()
-            new_task = values["tasks"] + '\n'
+            new_task = values["task"] + '\n'
             tasks.append(new_task)
             functions.write_tasks(tasks)
+            window['tasks'].update(values= tasks)
+        case 'Edit':
+            task_to_edit = values["tasks"][0]
+            new_task_in = values["task"]
+            tasks = functions.get_tasks()
+            index = tasks.index(task_to_edit)
+            tasks[index] = new_task_in + '\n'
+            functions.write_tasks(tasks)
+            window['tasks'].update(values= tasks)
+        case 'tasks':
+            window['task'].update(value= values["tasks"][0])
+
         case sg.WIN_CLOSED:
             break
 
