e0d4a405235efaa77fcec9a41e62ced3132028b5
Chukwunonso <nemelumadu10@gmail.com>
2025-03-20 13:37:19 +0100

    Implemented 'Complete' and 'Exit' feature

--- Gui.py
+++ Gui.py
@@ -7,9 +7,12 @@ new_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_tasks(), key= "tasks",
                       enable_events= True, size=(25,15))
 new_button2 = sg.Button("Edit")
+new_button3 = sg.Button("Complete")
+new_button4 = sg.Button("Exit")
 
 window = sg.Window("My to-do app",
-                   layout=[[label], [input_box], [new_button], [list_box,new_button2]],
+                   layout=[[label], [input_box], [new_button],
+                           [list_box,new_button2, new_button3], [new_button4]],
                    font= ('Helvetica', 20))
 while True:
     event, values = window.read()
@@ -22,6 +25,7 @@ while True:
             tasks.append(new_task)
             functions.write_tasks(tasks)
             window['tasks'].update(values= tasks)
+            window['task'].update(value="")
         case 'Edit':
             task_to_edit = values["tasks"][0]
             new_task_in = values["task"]
@@ -30,8 +34,19 @@ while True:
             tasks[index] = new_task_in + '\n'
             functions.write_tasks(tasks)
             window['tasks'].update(values= tasks)
+            window['task'].update(value="")
+        case 'Complete':
+            tasks = functions.get_tasks()
+            task_to_remove = values["tasks"][0]
+            index = tasks.index(task_to_remove)
+            tasks.pop(index)
+            functions.write_tasks(tasks)
+            window['tasks'].update(values=tasks)
+            window['task'].update(value="")
         case 'tasks':
             window['task'].update(value= values["tasks"][0])
+        case 'Exit':
+            break
 
         case sg.WIN_CLOSED:
             break
